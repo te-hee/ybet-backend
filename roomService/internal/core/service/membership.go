3422f0901a34cb8fc2a266530ff2b6b20918bbe4
gofer <dawidmarkiewicz214@gmail.com>
2026-03-18 21:03:28 +0100

    finished get room members function

--- roomService/internal/core/service/membership.go
+++ roomService/internal/core/service/membership.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"roomService/internal/contextkeys"
 	"roomService/internal/core/domain"
+	"slices"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *roomService) GetRoomMembers(ctx context.Context, roomUUID string) ([]domain.RoomMember, error) {
@@ -13,9 +17,22 @@ func (s *roomService) GetRoomMembers(ctx context.Context, roomUUID string) ([]do
 		return members, err
 	}
 
+	userRooms, err := s.repo.GetUserRooms(ctx, userID)
+	if err != nil {
+		return members, err
+	}
+
+	isMember := slices.ContainsFunc(userRooms, func(room domain.UserRoom) bool {
+		return room.RoomUUID == roomUUID
+	})
+
+	if !isMember {
+		return members, status.Error(codes.PermissionDenied, "Only room members can check other members")
+	}
+
 	members, err = s.repo.GetRoomMembers(ctx, roomUUID)
 	if err != nil {
-
+		return members, err
 	}
 	return members, nil
 }
